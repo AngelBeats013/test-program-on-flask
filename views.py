bb8b35410c7339af90e20c0d614085fb9d71b59c
weisen_cheng <cheng.weisen013@gmail.com>
2018-03-15 11:23:19 -0500

    finish directory

---
+++ views.py
@@ -0,0 +1,17 @@
+# Core logic
+# coding:utf8
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Login
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8080)
